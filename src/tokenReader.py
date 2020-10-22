bc51cf26feaf097efb754e268b2daebc2593eb3b
Jakub Kovář <kovar@netgrif.com>
2020-10-22 19:56:56 +0200

    restructured project to work with python demo bot implementation

--- src/tokenReader.py
+++ src/tokenReader.py
@@ -2,8 +2,8 @@ from os import path
 
 
 def getClientToken():
-    if not path.exists("resources/client-token"):
+    if not path.exists("../resources/client-token"):
         raise Exception("No client token file found! Make sure you have a token for the discord bot and it is located in a file 'resources/client-token'.")
 
-    f = open("resources/client-token", "r")
+    f = open("../resources/client-token", "r")
     return f.readline().strip()
